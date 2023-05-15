14a18681a0d4a0e2b21756f6366fa0c535c16675
JAMl3 <97791913+JAMl3@users.noreply.github.com>
2023-05-15 12:00:00 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os
 import PyPDF2
 from flask import Flask, render_template, request, flash, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+from flask import send_file
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Secret!123'
 
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        folder_path = request.form['folder_path']
-        unlocked_folder_path = os.path.join(folder_path, 'Unlocked PDFs')
+        unlocked_files = []
 
-        # Create the Unlocked PDFs folder if it doesn't exist
-        os.makedirs(unlocked_folder_path, exist_ok=True)
+        # Loop through each uploaded file
+        for uploaded_file in request.files.getlist('pdf_files'):
+            if uploaded_file.filename == '':
+                continue
 
-        # Loop through each file in the folder
-        for filename in os.listdir(folder_path):
-            filepath = os.path.join(folder_path, filename)
+            # Save the uploaded file to a secure location
+            filename = secure_filename(uploaded_file.filename)
+            filepath = os.path.join(app.root_path, 'uploads', filename)
+            uploaded_file.save(filepath)
 
             # Check if the file is a PDF
-            if os.path.isfile(filepath) and filename.lower().endswith('.pdf'):
+            if filename.lower().endswith('.pdf'):
                 # Open the password-protected PDF file
                 pdf_file = open(filepath, 'rb')
                 pdf_reader = PyPDF2.PdfFileReader(pdf_file)
@@ -35,7 +40,7 @@ def home():
 
                 # Save the new PDF file without any password or permission restrictions
                 output_filename = os.path.splitext(filename)[0] + '_Unlocked.pdf'
-                output_filepath = os.path.join(unlocked_folder_path, output_filename)
+                output_filepath = os.path.join(app.root_path, 'uploads', output_filename)
                 output_file = open(output_filepath, 'wb')
                 pdf_writer.write(output_file)
 
@@ -43,14 +48,18 @@ def home():
                 pdf_file.close()
                 output_file.close()
 
-        flash('PDF files have been unlocked and saved in the "Unlocked PDFs" folder', 'success')
-        return redirect('/')
+                unlocked_files.append(output_filename)
+
+        flash('PDF files have been unlocked.', 'success')
+        return render_template('home.html', unlocked_files=unlocked_files)
+
     return render_template('home.html')
 
 @app.route('/download/<path:filename>')
 def download_file(filename):
-    uploads = os.path.join(app.root_path, 'uploads/Unlocked PDFs')
-    return send_from_directory(directory=uploads, filename=filename)
+    uploads = os.path.join(app.root_path, 'uploads')
+    return send_file(os.path.join(uploads, filename), as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
